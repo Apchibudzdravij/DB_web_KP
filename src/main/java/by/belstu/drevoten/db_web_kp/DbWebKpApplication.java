1e78fb2a92f3fc39d0c2afd284e50ae50a7874c3
Apchibudzdravij <drevoteny@gmail.com>
2022-10-31 20:22:34 +0300

    Project started

---
+++ src/main/java/by/belstu/drevoten/db_web_kp/DbWebKpApplication.java
@@ -0,0 +1,13 @@
+package by.belstu.drevoten.db_web_kp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbWebKpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbWebKpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/belstu/drevoten/db_web_kp/DbWebKpApplicationTests.java
@@ -0,0 +1,13 @@
+package by.belstu.drevoten.db_web_kp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbWebKpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
